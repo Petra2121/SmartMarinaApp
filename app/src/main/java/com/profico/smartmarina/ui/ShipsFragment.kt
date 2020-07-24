cf8b126c7e8228a8ee63fcb5e52fb47b583d1209
mihaela-w <mwang00@fesb.hr>
2020-07-24 11:34:29 +0200

    dodaju se brodovi i idu na home screen

--- app/src/main/java/com/profico/smartmarina/ui/ShipsFragment.kt
+++ app/src/main/java/com/profico/smartmarina/ui/ShipsFragment.kt
@@ -2,12 +2,12 @@ package com.profico.smartmarina.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.View
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.profico.smartmarina.ListAdapter
 import com.profico.smartmarina.R
 import com.profico.smartmarina.data.model.DataShips
-import kotlinx.android.synthetic.main.fragment_add_ship.*
 import kotlinx.android.synthetic.main.fragment_ships.*
 
 /**
@@ -15,18 +15,39 @@ import kotlinx.android.synthetic.main.fragment_ships.*
  */
 class ShipsFragment : BaseFragment() {
 
+    val args : ShipsFragmentArgs by navArgs()
+
+    var shipsAdapter : ListAdapter? = null
+
     override fun getLayout(): Int = R.layout.fragment_ships
 
     override fun hasToolbar(): Boolean = false
 
     override fun setupView() {
-
         btnAddNewShip.setOnClickListener {
             findNavController().navigate(ShipsFragmentDirections.actionShipsFragmentToAddShipFragment())
         }
+
+        recyclerShips.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            shipsAdapter = ListAdapter(ShipsList) {
+                findNavController().navigate(ShipsFragmentDirections.actionShipsFragmentToHomeScreenFragment())
+
+            }
+
+            adapter = shipsAdapter
+        }
+
+        if (args.shipName2 != "") {
+            ShipsList.add(DataShips(args.shipName2))
+            shipsAdapter?.notifyDataChange(ShipsList)
+        }
     }
 
-    private val ShipsList = listOf(
+    private val ShipsList = mutableListOf(
         DataShips("National Treasure"),
         DataShips("Thunder"),
         DataShips("Blue Ocean")
@@ -35,18 +56,6 @@ class ShipsFragment : BaseFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        recyclerShips.apply {
-            // set a LinearLayoutManager to handle Android
-            // RecyclerView behavior
-            layoutManager = LinearLayoutManager(activity)
-            // set the custom adapter to the RecyclerView
-            adapter = com.profico.smartmarina.ListAdapter(ShipsList)
-        }
     }
 
     companion object {
